d65503dc27fe7da3fd53197fe372914dc33eac03
M@W <mehdicr7wahid@gmail.com>
2022-10-03 19:21:08 +0430

    upgraded the next version

--- components/PatientSocialInfoContainer.jsx
+++ components/PatientSocialInfoContainer.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import PatientSocialInfo from './patientsSocialInfoComponent'
-import ValueForm from './valueForm'
+import React from "react";
+import PatientSocialInfo from "./patientsSocialInfoComponent";
 
-const PatientSocial = ({patientData}) => {
+const PatientSocial = ({ patientData }) => {
   return (
-    <div className='bg-slate-700 rounded md:order-3 md:my-5 lg:my-0 md:px-8 lg:px-0  md:mx-auto lg:mx-0 lg:order-none'>
-        {/* first row*/}
-        <div className="flex gap-7 px-10 pt-10 ">
+    <div className="bg-slate-700 rounded md:order-3 md:my-5 lg:my-0 md:px-8 lg:px-0  md:mx-auto lg:mx-0 lg:order-none">
+      {/* first row*/}
+      <div className="flex gap-7 px-10 pt-10 ">
         {/* Gender */}
         <PatientSocialInfo label="Gender" value={patientData?.sex} />
         {/* Marital Stauts */}
-        <PatientSocialInfo label="Marital Status" value={patientData?.marital} />
+        <PatientSocialInfo
+          label="Marital Status"
+          value={patientData?.marital}
+        />
         {/* phone  */}
         <PatientSocialInfo label="Phone" value={patientData?.phone_number} />
       </div>
@@ -22,47 +24,72 @@ const PatientSocial = ({patientData}) => {
         {/* Job */}
         <PatientSocialInfo label="Job" value={patientData?.job} />
         {/* Registry Date */}
-        <PatientSocialInfo label="Registry Date" value={patientData?.registeredAt} />
+        <PatientSocialInfo
+          label="Registry Date"
+          value={patientData?.registeredAt}
+        />
       </div>
       {/* third row  */}
       <div className="flex gap-7 px-10 pt-5">
-      {/* HIV */}
-      <PatientSocialInfo label="HIV" value={patientData?.hiv ? 'True' : "False"} />
-       {/* HBS */}
-       <PatientSocialInfo label="HBS" value={patientData?.hbs ? 'True' : "False"} />
+        {/* HIV */}
+        <PatientSocialInfo
+          label="HIV"
+          value={patientData?.hiv ? "True" : "False"}
+        />
+        {/* HBS */}
+        <PatientSocialInfo
+          label="HBS"
+          value={patientData?.hbs ? "True" : "False"}
+        />
         {/* HCV */}
-        <PatientSocialInfo label="HCV" value={patientData?.hcv ? 'True' : "False"} />
+        <PatientSocialInfo
+          label="HCV"
+          value={patientData?.hcv ? "True" : "False"}
+        />
       </div>
 
       {/* fourth row  */}
       <div className="px-10 py-5 flex gap-7">
         {/* pregnancy */}
-        <ValueForm
+        <PatientSocialInfo
+          label="Pregnancy"
+          value={patientData?.pregnancy ? "True" : "False"}
+        />
+        {/* <ValueForm
           name="pregnancy"
           type="text"
           data={patientData?.pregnancy}
           label="Pregnancy"
           disabled={true}
+        /> */}
+        {/* diabetes */}
+        <PatientSocialInfo
+          label="Diabetes"
+          value={patientData?.diabetes ? "True" : "False"}
         />
-          {/* diabetes */}
-        <ValueForm
+        {/* <ValueForm
           name="diabetes"
           type="text"
           data={patientData?.diabetes}
           label="Diabetes"
           disabled={true}
-        />
+        /> */}
         {/* reflux */}
+        <PatientSocialInfo
+          label="Reflux Esophagitis"
+          value={patientData?.reflux ? "True" : "False"}
+        />
+        {/* 
         <ValueForm
           name="reflux"
           type="text"
           data={patientData?.reflux}
           label="Reflux Esophagits"
           disabled={true}
-        />
+        /> */}
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default PatientSocial
+export default PatientSocial;
